0602030c2c38c92d8922c732b6b40f7ba0ffeaad
Jim Braun <jbraun@icecube.wisc.edu>
2004-06-10 08:46:07 -0500

    Added functionality to initiate test sequence with java client

--- src/icecube/daq/domcal/DOMCal.java
+++ src/icecube/daq/domcal/DOMCal.java
@@ -14,7 +14,6 @@ package icecube.daq.domcal;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.BasicConfigurator;
-import org.w3c.dom.Document;
 
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -27,21 +26,76 @@ public class DOMCal implements Runnable {
 
     private static Logger logger = Logger.getLogger( "domcal" );
 
-    public static final int RECORD_LENGTH = 9384;
+    public static final int WAIT_UNSPECIFIED = -2;
 
     private String host;
     private int port;
-    private String outFile;
+    private String outDir;
+    private int waitTime;
 
-    public DOMCal( String host, int port, String outFile ) {
+    public DOMCal( String host, int port, String outDir ) {
         this.host = host;
         this.port = port;
-        this.outFile = outFile;
+        this.outDir = outDir;
+        if ( !outDir.endsWith( "/" ) ) {
+            outDir += "/";
+        }
+        this.waitTime = WAIT_UNSPECIFIED;
+    }
+
+    public DOMCal( String host, int port, String outDir, int waitTime ) {
+        this.host = host;
+        this.port = port;
+        this.outDir = outDir;
+        if ( !outDir.endsWith( "/" ) ) {
+            outDir += "/";
+        }
+        this.waitTime = waitTime;
     }
 
     public void run() {
 
         Socket s = null;
+        DOMCalCom com = null;
+
+        /* Start test if waitTime is set */
+        if ( waitTime != WAIT_UNSPECIFIED ) {
+
+            try {
+                s = new Socket( host, port );
+            } catch ( UnknownHostException e ) {
+                logger.error( "Cannot connect to " + host );
+                die( e );
+            } catch ( IOException e ) {
+                logger.error( "IO Error connecting to " + host );
+                die( e );
+            }
+
+            logger.debug( "Connected to " + host + " at port " + port );
+
+            try {
+                com = new DOMCalCom( s );
+            } catch ( IOException e ) {
+                logger.error( "IO Error establishing communications" );
+            }
+
+            logger.info( "Beginning DOM calibration routine" );
+            try {
+                com.send( "s\" domcal\" find if exec endif" );
+                com.receive( "\r\n" );
+                com.close();
+            } catch ( IOException e ) {
+                logger.error( "IO Error starting DOM calibration routine" );
+                die( e );
+            }
+
+            try {
+                Thread.sleep( waitTime );
+            } catch ( InterruptedException e ) {
+                logger.error( "Interrupted from sleep -- quitting" );
+                die( e );
+            }
+        }
 
         try {
             s = new Socket( host, port );
@@ -54,8 +108,6 @@ public class DOMCal implements Runnable {
         }
 
         logger.debug( "Connected to " + host + " at port " + port );
-
-        DOMCalCom com = null;
 
         try {
             com = new DOMCalCom( s );
@@ -77,6 +129,8 @@ public class DOMCal implements Runnable {
         }
 
         DOMCalRecord rec = null;
+        String domId = rec.getDomId();
+
         try {
             rec = DOMCalRecordFactory.parseDomCalRecord( ByteBuffer.wrap( binaryData ) );
         } catch ( Exception e ) {
@@ -86,10 +140,10 @@ public class DOMCal implements Runnable {
 
         String xmlDoc = DOMCalXML.format( rec );
 
-        logger.debug( "Saving output to " + outFile );
+        logger.debug( "Saving output to " + outDir );
 
         try {
-            PrintWriter out = new PrintWriter( new FileWriter( outFile, false ), false );
+            PrintWriter out = new PrintWriter( new FileWriter( outDir + domId + ".xml", false ), false );
             out.print( xmlDoc );
             out.flush();
             out.close();
@@ -99,27 +153,69 @@ public class DOMCal implements Runnable {
         }
 
         logger.debug( "Document saved" );
-
     }
 
     public static void main( String[] args ) {
         BasicConfigurator.configure();
         String host = null;
         int port = -1;
-        String outFile = null;
+        int nPorts = -1;
+        int waitTime = -1;
+        String outDir = null;
         try {
-            host = args[0];
-            port = Integer.parseInt( args[1] );
-            outFile = args[2];
+            if ( args.length == 3 ) {
+                host = args[0];
+                port = Integer.parseInt( args[1] );
+                outDir = args[2];
+                ( new DOMCal( host, port, outDir ) ).run();
+            } else if ( args.length == 4 ) {
+                host = args[0];
+                port = Integer.parseInt( args[1] );
+                nPorts = Integer.parseInt( args[2] );
+                outDir = args[3];
+                for ( int i = 0; i < nPorts; i++ ) {
+                    ( new Thread( new DOMCal( host, port + i, outDir ), "" + port + i ) ).start();
+                }
+            } else if ( args.length == 5 ) {
+                host = args[0];
+                port = Integer.parseInt( args[1] );
+                outDir = args[2];
+                waitTime = Integer.parseInt( args[4] );
+                ( new DOMCal( host, port, outDir, waitTime ) ).run();
+            } else if ( args.length == 6 ) {
+                host = args[0];
+                port = Integer.parseInt( args[1] );
+                nPorts = Integer.parseInt( args[2] );
+                outDir = args[3];
+                waitTime = Integer.parseInt( args[5] );
+                for ( int i = 0; i < nPorts; i++ ) {
+                    ( new Thread( new DOMCal( host, port + i, outDir, waitTime ), "" + port + i ) ).start();
+                }
+            } else {
+                usage();
+                die( "Invalid command line arguments" );
+            }
         } catch ( Exception e ) {
             usage();
             die( e );
         }
-        ( new DOMCal( host, port, outFile ) ).run();
     }
 
     private static void usage() {
-        logger.info( "DOMCal Usage: java icecube.daq.domcal.DOMCal {host} {port}" );
+        logger.info( "DOMCal Usage: java icecube.daq.domcal.DOMCal {host} {port} {output dir}" );
+        logger.info( "DOMCal Usage: java icecube.daq.domcal.DOMCal {host} {port} {output dir} wait {seconds}" );
+        logger.info( "DOMCal Usage: java icecube.daq.domcal.DOMCal {host} {port} {num ports} {output dir}" );
+        logger.info( "DOMCal Usage: java icecube.daq.domcal.DOMCal {host} {port} {num ports}" +
+                                                                      "{output dir} wait {seconds}" );
+        logger.info( "host -- hostname of domhub/terminal server to connect" );
+        logger.info( "port -- remote port to connect on 'host'" );
+        logger.info( "num ports -- number of sequential ports to connect above 'port' on 'host'" );
+        logger.info( "output dir -- local directory to store results" );
+        logger.info( "wait -- flag to initiate DOM calibration" );
+        logger.info( "seconds -- time to wait after initiating a calibration before downloading data " +
+                                                                                      "-- a good value is 120" );
+        
+
     }
 
     private static void die( Object o ) {
