e8af60246eebd1417f5b91835bc11b56c1d5eaa5
Kael Hanson <kael.hanson@gmail.com>
2004-06-25 09:12:22 -0500

    Adding to CVS control in dom-cal module

---
+++ src/icecube/daq/domcal/DOMCalibrationException.java
@@ -0,0 +1,21 @@
+package icecube.daq.domcal;
+
+/**
+ *  Calibrator exception class.
+ *
+ *  @author kael
+ *
+ */
+public class DOMCalibrationException extends Exception {
+
+    /**
+     * Constructor
+     */
+    public DOMCalibrationException(String msg) {
+
+        super(msg);
+
+    }
+
+
+}
