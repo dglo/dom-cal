7453368043b5e5dfbcc92d5564382cb933a65e75
John Kelley <jkelley@icecube.wisc.edu>
2004-06-30 11:11:09 -0500

    Fixed horrible bug in ATWD1 calibration -- was a copy of ATWD0 data

--- private/dom-cal/atwd_cal.c
+++ private/dom-cal/atwd_cal.c
@@ -93,10 +93,9 @@ int atwd_cal(calib_data *dom_calib) {
                 }
             
                 /* Sum the waveform */
-                for(atwd=0; atwd<2; atwd++)
-                    for(ch=0; ch<3; ch++)
-                        for(bin=0; bin<cnt; bin++)
-                            atwd_pedestal[atwd][ch][bin][bias_idx] += channels[ch][bin];  
+                for(ch=0; ch<3; ch++)
+                    for(bin=0; bin<cnt; bin++)
+                        atwd_pedestal[atwd][ch][bin][bias_idx] += channels[ch][bin];  
 
             }
         }
