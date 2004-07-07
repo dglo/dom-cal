0d43ae2fcfdc957b2a1658774a5081ec968990c7
Jim Braun <jbraun@icecube.wisc.edu>
2004-07-07 10:41:59 -0500

    *** empty log message ***

--- src/icecube/daq/domcal/DOMCal.java
+++ src/icecube/daq/domcal/DOMCal.java
@@ -33,7 +33,7 @@ public class DOMCal implements Runnable {
     public static final int TIMEOUT = 600;
    
     /* DOMCal version ID */
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "2.0";
 
     private static Logger logger = Logger.getLogger( "domcal" );
     private static List threads = new LinkedList();
