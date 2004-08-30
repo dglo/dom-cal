3530804ed033f38dcdf54c1f94de5af8908f9996
Jim Braun <jbraun@icecube.wisc.edu>
2004-08-30 05:57:03 -0500

    increased client timeout to 40 minutes

--- src/icecube/daq/domcal/DOMCal.java
+++ src/icecube/daq/domcal/DOMCal.java
@@ -25,7 +25,7 @@ import java.sql.*;
 public class DOMCal implements Runnable {
     
     /* Timeout waiting for response, in seconds */
-    public static final int TIMEOUT = 1200;
+    public static final int TIMEOUT = 2400;
    
     private static Logger logger = Logger.getLogger( DOMCal.class );
     private static List threads = new LinkedList();
