a92fc982fec7f62552a22be9b5da494c6cd29d7a
CVS-to-SVN conversion program <cvs2svn@icecube.wisc.edu>
2004-09-09 09:00:50 -0500

    This commit was manufactured by cvs2svn to create tag 'V01-01-00'.

--- src/icecube/daq/domcal/HVHistogramGrapher.java
+++ src/icecube/daq/domcal/HVHistogramGrapher.java
@@ -29,8 +29,11 @@ import java.awt.*;
 
 import com.sun.image.codec.jpeg.JPEGCodec;
 import com.sun.image.codec.jpeg.JPEGImageEncoder;
+import com.sun.image.codec.jpeg.JPEGEncodeParam;
 
 public class HVHistogramGrapher implements Runnable {
+
+    public static final double EC = 1.6022e-19;
 
     private String inDir;
     private String outDir;
@@ -87,8 +90,10 @@ public class HVHistogramGrapher implements Runnable {
         }
 
         HTMLDoc doc = new HTMLDoc(outDir + (outDir.endsWith("/") ? "" : "/") + "hv.html");
+        HTMLDoc sumDoc = new HTMLDoc(outDir + (outDir.endsWith("/") ? "" : "/") + "hvsummary.html");
         try {
             doc.open();
+            sumDoc.open();
         } catch (IOException e) {
             System.out.println("IOException opening output HTML file " + e);
             return;
@@ -102,22 +107,47 @@ public class HVHistogramGrapher implements Runnable {
         doc.add("1700V");
         doc.add("1800V");
         doc.add("1900V");
+        doc.add("Gain vs HV");
         doc.addBr();
+        sumDoc.add("DOM Id");
+        sumDoc.add("1200V");
+        sumDoc.add("1300V");
+        sumDoc.add("1400V");
+        sumDoc.add("1500V");
+        sumDoc.add("1600V");
+        sumDoc.add("1700V");
+        sumDoc.add("1800V");
+        sumDoc.add("1900V");
+        sumDoc.add("Gain vs HV");
+        sumDoc.addBr();
         for (Iterator it = histTable.keySet().iterator(); it.hasNext();) {
             String domId = (String)it.next();
             HVHistogram[] histArr = (HVHistogram[])histTable.get(domId);
             StringTokenizer st = new StringTokenizer(domId, ".xml");
-            doc.addNew(st.nextToken() + "\t");
+            String id = st.nextToken();
+            doc.addNew(id);
+            sumDoc.addNew(id);
             for (int i = 0; i < histArr.length; i++) {
                 try {
-                    doc.addImg(graphHistogram(histArr[i], domId));
+                    String loc = graphHistogram(histArr[i], domId);
+                    doc.addImg(loc);
+                    sumDoc.addSizedImg(loc, 100, 100);
                 } catch (IOException e) {
                     System.out.println("Failed encoding histogram " + e);
                 }
             }
+            try {
+                String loc = graphHV(histArr, domId);
+                doc.addImg(loc);
+                sumDoc.addSizedImg(loc, 100, 100);
+            } catch (IOException e) {
+                System.out.println("Failed encoding hv summary " + e);
+            }
             doc.addBr();
+            sumDoc.addBr();
         }
         doc.close();
+        sumDoc.close();
     }
 
     public HVHistogram[] processDomcal(File domcalFile) throws ParserConfigurationException, SAXException, IOException {
@@ -140,8 +170,143 @@ public class HVHistogramGrapher implements Runnable {
         String outHttp = htmlRoot + (htmlRoot.endsWith("/") ? "" : "/") + outName;
         BufferedImage bi = createImage(histo);
         JPEGImageEncoder jout = JPEGCodec.createJPEGEncoder(new FileOutputStream(outFile));
+        JPEGEncodeParam ep = jout.getDefaultJPEGEncodeParam(bi);
+        ep.setQuality(2, false);
+        jout.setJPEGEncodeParam(ep);
         jout.encode(bi);
+        System.out.println(outFile);
         return outHttp;
+    }
+
+    private String graphHV(HVHistogram[] histos, String domId) throws IOException {
+        String outName = domId + "_hv" + ".jpeg";
+        String outFile = outDir + (outDir.endsWith("/") ? "" : "/") + outName;
+        String outHttp = htmlRoot + (htmlRoot.endsWith("/") ? "" : "/") + outName;
+        BufferedImage bi = createSummaryImage(histos);
+        JPEGImageEncoder jout = JPEGCodec.createJPEGEncoder(new FileOutputStream(outFile));
+        JPEGEncodeParam ep = jout.getDefaultJPEGEncodeParam(bi);
+        ep.setQuality(2, false);
+        jout.setJPEGEncodeParam(ep);
+        jout.encode(bi);
+        System.out.println(outFile);
+        return outHttp;
+    }
+
+    private BufferedImage createSummaryImage(HVHistogram[] histos) {
+        BufferedImage bi = new BufferedImage(300, 300, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = bi.createGraphics();
+
+        //set background white
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, bi.getWidth(), bi.getHeight());
+
+        //draw axes
+        g.setColor(Color.BLACK);
+        g.drawLine(50, 249, 299, 249);
+        g.drawLine(50, 0, 50, 249);
+
+        //draw X tick marks
+        int[] xTicks = {1200, 1400, 1600, 1800, 2000};
+        for (int i = 0; i < xTicks.length; i ++) {
+            int x = getXPixel(xTicks[i]);
+            g.drawLine(x, 247, x, 251);
+        }
+
+        //draw Y tick marks
+        int[] yTicks = {1000000, 3000000, 10000000, 30000000, 100000000};
+        String[] yTickStr = {"1e6", "3e6", "1e7", "3e7", "1e8"};
+        for (int i = 0; i < yTicks.length; i++) {
+            int y = getYPixel(yTicks[i]);
+            g.drawLine(48, y, 52, y);
+        }
+
+        //create Y labels
+        int charHeight = g.getFontMetrics().getHeight();
+        for (int i = 0; i < yTicks.length; i++) {
+            g.drawString("" + yTickStr[i],
+                       25 - (g.getFontMetrics().stringWidth("" + yTickStr[i]))/2, getYPixel(yTicks[i]) + charHeight/2);
+        }
+
+        //create X labels
+        for (int i = 0; i < xTicks.length; i++) {
+            g.drawString("" + xTicks[i], getXPixel(xTicks[i]) -
+                                    (g.getFontMetrics().stringWidth("" + xTicks[i]))/2, 249 + (50 + charHeight)/2);
+        }
+
+        //count number of good fits
+        int conv = 0;
+        for (int i = 0; i < histos.length; i++) {
+            if (histos[i].isConvergent()) conv++;
+        }
+
+        //allocate arrays for linear fit
+        int[] xData = new int[conv];
+        int[] yData = new int[conv];
+
+        //plot data
+        int convIndx = 0;
+        g.setColor(Color.RED);
+        for (int i = 0; i < histos.length; i++) {
+            if (histos[i].isConvergent()) {
+                int xCenter = getXPixel(histos[i].getVoltage());
+                int yCenter = getYPixel((int)(1e-12 * histos[i].getFitParams()[3] / EC));
+                if (xCenter > 296) xCenter = 296;
+                if (xCenter < 53) xCenter = 53;
+                if (yCenter > 246) yCenter = 246;
+                if (yCenter < 3) yCenter = 3;
+                g.drawLine(xCenter - 3, yCenter, xCenter + 3, yCenter);
+                g.drawLine(xCenter, yCenter - 3, xCenter, yCenter + 3);
+                xData[convIndx] = xCenter;
+                yData[convIndx] = yCenter;
+                convIndx++;
+            }
+        }
+
+        //do equivalent regression to domcal
+        if (conv >1) {
+            LinearFit fit = null;
+            try {
+                fit = fit(xData, yData);
+            } catch (FitException e) {
+                System.out.println("Fit error " + e);
+            }
+
+            //remember we're dealing with pixels here!
+            if (fit != null) {
+                g.setColor(Color.GREEN);
+                int startX = 51;
+                int endX = 299;
+                int startY = (int)(fit.getYIntercept() + startX*fit.getSlope());
+                //find where line reaches 51
+                while (startY < 51 && startX < endX) {
+                    startX++;
+                    startY = (int)(fit.getYIntercept() + startX*fit.getSlope());
+                }
+                int endY = (int)(fit.getYIntercept() + endX*fit.getSlope());
+                while (endY > 299 && startX < endX) {
+                    endX--;
+                    endY = (int)(fit.getYIntercept() + endX*fit.getSlope());
+                }
+                if (startX != endX) g.drawLine(startX, startY, endX, endY);
+            }
+        }
+        return bi;
+    }
+
+    private int getXPixel(int val) {
+        double logVal = Math.log(val);
+        double staticHVal = Math.log(1900);
+        double staticLVal = Math.log(1100);
+
+        return 50 + (int)(200 * ((logVal - staticLVal)/(staticHVal - staticLVal)));
+    }
+
+    private int getYPixel(int val) {
+        double logVal = Math.log(val);
+        double staticHVal = Math.log(1e8);
+        double staticLVal = Math.log(1e6);
+
+        return 249 - (int)(220 * ((logVal - staticLVal)/(staticHVal - staticLVal)));
     }
 
     private BufferedImage createImage(HVHistogram histo) {
@@ -166,6 +331,26 @@ public class HVHistogramGrapher implements Runnable {
             g.drawLine(50 + i, 249, 50 + i, 249 - (int)cnt);
         }
 
+        //draw fit
+        if (histo.isConvergent()) {
+            g.setColor(Color.GREEN);
+        } else {
+            g.setColor(Color.RED);
+        }
+        float[] fp = histo.getFitParams();
+        int fitPrevious = -1;
+        for (int i = 0; i < 250; i++) {
+            float x = xData[i];
+            int fitY = (int)((fp[0]*Math.exp(-1*fp[1]*x))+(fp[2]*Math.exp(-1*Math.pow((double)(x - fp[3]), 2)*fp[4])));
+            if (fitY < 1) fitY = 1;
+            else if (fitY > 247) fitY = 247;
+            g.drawLine(50 + i, 249-fitY-1, 50 + i, 249-fitY+1);
+            if (i > 0 && fitPrevious != -1) {
+                g.drawLine(50 + i, 249-fitPrevious, 50 + i, 249-fitY);
+            }
+            fitPrevious = fitY;
+        }
+
         g.setColor(Color.WHITE);
         //draw axes
         g.drawLine(50, 249, 299, 249);
@@ -186,23 +371,107 @@ public class HVHistogramGrapher implements Runnable {
             g.drawString(str, 50 + 50*i - (g.getFontMetrics().stringWidth(str))/2, 249 + (50 + charHeight)/2);
         }
 
-        //draw fit
-        if (histo.isConvergent()) {
-            g.setColor(Color.GREEN);
-        } else {
-            g.setColor(Color.RED);
-        }
-        float[] fp = histo.getFitParams();
-        for (int i = 0; i < 250; i++) {
-            float x = xData[i];
-            int fitY = (int)((fp[0]*Math.exp(-1*fp[1]*x))+(fp[2]*Math.exp(-1*Math.pow((double)(x - fp[3]), 2)*fp[4])));
-            if (fitY < 0) fitY = 0;
-            else if (fitY > 247) fitY = 247;
-            g.drawLine(50 + i, 249-fitY-1, 50 + i, 249-fitY+1);
-        }
-
         return bi;
     }
+
+    private LinearFit fit( int[] xData, int[] yData ) throws FitException {
+
+        int length = xData.length;
+
+        if ( length != yData.length ) {
+            throw new FitException( FitException.EX_LENGTH_NOT_Y_LENGTH );
+        }
+
+        if ( length < 2 ) {
+            throw new FitException( FitException.EDATA_LENGTH );
+        }
+
+        int max = xData[0];
+        int min = xData[0];
+        for ( int i = 0; i < length; i++ ) {
+            if ( xData[i] > max ) {
+                max = xData[i];
+            } else if ( xData[i] < min ) {
+                min = xData[i];
+            }
+        }
+
+        if ( max == min ) {
+            throw new FitException( FitException.EVERTICAL_LINE );
+        }
+
+        //OK...Ruled out any show stoppers...on to the fit!
+        //Credit John Kelly with this routine
+
+        int sumX = 0;
+        int sumY = 0;
+        int sumXX = 0;
+        int sumYY = 0;
+        int sumXY = 0;
+
+        for ( int i = 0; i < length; i++ ) {
+
+            sumX += xData[i];
+            sumXX += xData[i] * xData[i];
+            sumY += yData[i];
+            sumYY += yData[i] * yData[i];
+            sumXY += yData[i] * xData[i];
+
+        }
+
+        float denom = ( length * sumXX ) - ( sumX * sumX );
+
+        float slope = ( float )( ( length * sumXY )  - ( sumX * sumY ) ) / denom;
+        float yIntercept = ( float )( ( sumXX * sumY ) - ( sumX * sumXY ) ) / denom;
+        float rSquared = ( float )( ( ( length * sumXY ) - ( sumX * sumY ) ) * slope /
+                                                         ( ( length * sumYY ) - ( sumY * sumY ) ) );
+
+        return new LinearFit( slope, yIntercept, rSquared );
+
+    }
+
+    private class LinearFit {
+
+            private float slope;
+            private float yIntercept;
+            private float rSquared;
+
+            public LinearFit( float slope, float yIntercept, float rSquared ) {
+                this.slope = slope;
+                this.yIntercept = yIntercept;
+                this.rSquared = rSquared;
+            }
+
+            public float getSlope() {
+                return slope;
+            }
+
+            public float getYIntercept() {
+                return yIntercept;
+            }
+
+            public float getRSquared() {
+                return rSquared;
+            }
+        }
+
+        private class FitException extends Exception {
+
+            public static final int EVERTICAL_LINE = -1;
+            public static final int EDATA_LENGTH = -2;
+            public static final int EX_LENGTH_NOT_Y_LENGTH = -3;
+
+            private int condition;
+
+            public FitException( int condition ) {
+                this.condition = condition;
+            }
+
+            public int getCondition() {
+                return condition;
+            }
+        }
+
 
     private static class HTMLDoc {
 
@@ -217,7 +486,7 @@ public class HVHistogramGrapher implements Runnable {
         public void open() throws IOException {
             out = new PrintWriter(new FileWriter(pathName));
             out.println("<html>");
-            out.println("<table border=2 cellpadding=10 cellspacing=2 valign=top>");
+            out.println("<table border=2 cellpadding=9 cellspacing=2 valign=top>");
             out.println("<tr align=\"center\">");
         }
 
@@ -237,7 +506,12 @@ public class HVHistogramGrapher implements Runnable {
         }
 
         public void addImg(String path) {
-            add("<img src=" + path + ">");
+            add("<a href=\"" + path + "\"><img src=" + path + "></a>");
+        }
+
+        public void addSizedImg(String path, int height, int width) {
+            add("<a href=\"" + path + "\"><img height=\"" + height +
+                                                  "\" width=\"" + width + "\" src=" + path + "></a>");
         }
 
         public void close() {
