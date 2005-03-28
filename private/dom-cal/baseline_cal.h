d2fc74dfde10bd7730a45198e5015e84f22c69cd
Jim Braun <jbraun@icecube.wisc.edu>
2005-03-28 17:26:22 -0600

    Added baseline routineAdded baseline routineAdded baseline routineAdded baseline routineAdded baseline routineAdded baseline routineAdded baseline routineAdded baseline routineAdded baseline routine

---
+++ private/dom-cal/baseline_cal.h
@@ -0,0 +1,9 @@
+
+/* Number of pedestal patterns to average */
+#define BASELINE_CAL_TRIG_CNT       100
+
+/* Maximum variance of the baseline (for no waveform contamination) */
+#define BASELINE_CAL_MAX_VAR     3.0E-6
+
+int hv_baseline_cal(calib_data *dom_calib);
+int baseline_cal(calib_data *dom_calib);
