abfd8a083d41e0d7facd2fa2b05a3ccfb4a8eb2f
Jim Braun <jbraun@icecube.wisc.edu>
2005-06-16 05:28:18 -0500

    Optimize params for hv gain cal and amp cal LED settings

--- private/dom-cal/hv_amp_cal.h
+++ private/dom-cal/hv_amp_cal.h
@@ -13,16 +13,16 @@
 #define HV_AMP_CAL_MAX_PULSE 800
 
 /* Time constant for rate measurement */
-#define TEST_TRIG_TIME 7 //seconds
+#define TEST_TRIG_TIME 5 //seconds
 
 /* LED amplitude decrement */
-#define LED_AMPLITUDE_DEC 10
+#define LED_AMPLITUDE_DEC 5
 
 /* LED amplitude setting to denote LED is off */
 #define LED_OFF 0x6fff 
 
 /* Minimum acceptable rate of pulses capable of amp calibration */
-#define MIN_PULSE_RATE 7.0
+#define MIN_PULSE_RATE 8.0
 
 /* Maximum amplitude of LED */
 #define LED_MAX_AMPLITUDE 1023
