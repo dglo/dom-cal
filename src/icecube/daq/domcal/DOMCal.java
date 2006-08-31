6dde4a125ceeb7a22d3a2ef30bb0a760604f8e80
John Kelley <jkelley@icecube.wisc.edu>
2006-08-31 13:23:38 -0500

    Increase timeout to 2h

--- src/icecube/daq/domcal/DOMCal.java
+++ src/icecube/daq/domcal/DOMCal.java
@@ -27,7 +27,7 @@ import java.sql.ResultSet;
 public class DOMCal implements Runnable {
 
     /* Timeout waiting for response, in seconds */
-    public static final int TIMEOUT = 5400;
+    public static final int TIMEOUT = 7200;
 
     private static Logger logger = Logger.getLogger( DOMCal.class );
 
