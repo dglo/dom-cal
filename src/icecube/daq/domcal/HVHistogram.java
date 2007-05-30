f0ab5c47d93315dbd9a22ea13e5c2c55d1ac4ce5
CVS-to-SVN conversion program <cvs2svn@icecube.wisc.edu>
2007-05-30 11:19:13 -0500

    This commit was manufactured by cvs2svn to create tag 'V07-00-06b'.

--- src/icecube/daq/domcal/HVHistogram.java
+++ src/icecube/daq/domcal/HVHistogram.java
@@ -58,10 +58,17 @@ public class HVHistogram {
 
     public static HVHistogram parseHVHistogram(Element histo) {
         short voltage = Short.parseShort(histo.getAttribute("voltage"));
-        boolean convergent = (Short.parseShort(histo.getAttribute("convergent")) == 1);
+        // convergent and ifFilled attributes have changed format
+        boolean convergent, isFilled;
+        try {
+            convergent = (Short.parseShort(histo.getAttribute("convergent")) == 1);
+            isFilled = (Short.parseShort(histo.getAttribute("isFilled")) == 1);            
+        } catch (NumberFormatException e) {
+            convergent = Boolean.parseBoolean(histo.getAttribute("convergent"));
+            isFilled = Boolean.parseBoolean(histo.getAttribute("isFilled"));
+        }
         float pv = Float.parseFloat(histo.getAttribute("pv"));
         float noiseRate = Float.parseFloat(histo.getAttribute("noiseRate"));
-        boolean isFilled = (Short.parseShort(histo.getAttribute("isFilled")) == 1);
         float[] fitParams = new float[5];
         NodeList fitP = histo.getElementsByTagName("param");
         for (int i = 0; i < fitP.getLength(); i++) {
