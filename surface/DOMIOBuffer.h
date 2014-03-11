301054c2e53ac83904c8c1838d1e9efe25686c73
Chris Weaver <chris.weaver@icecube.wisc.edu>
2014-03-11 09:35:15 -0500

    Try to clean up the effects of a bad merge.

--- surface/DOMIOBuffer.h
+++ surface/DOMIOBuffer.h
@@ -13,9 +13,6 @@
 #include <iostream>
 
 class DOMIOBuffer : public std::basic_streambuf<char, std::char_traits<char> >{
-public:
-	typedef char char_type;
-	typedef int int_type;
 private:
 	int fd;
 	unsigned int minReadSize;
@@ -57,18 +54,18 @@ private:
 	}
 	
 public:
-       DOMIOBuffer(const std::string& devPath, unsigned int minReadSize):
-       fd(open(devPath.c_str(),O_RDWR|O_NONBLOCK)),
-       minReadSize(minReadSize),
-       readBuffer(new char_type[minReadSize]),
-       readEOF(false),
-       devicePath(devPath){
-           if(fd<0) {
-               perror(devPath.c_str());
-               throw std::runtime_error("Failed to open DOR device "+devPath);
-           }
-           setg(readBuffer,readBuffer,readBuffer);
-        }
+	DOMIOBuffer(const std::string& devPath, unsigned int minReadSize):
+	fd(open(devPath.c_str(),O_RDWR|O_NONBLOCK)),
+	minReadSize(minReadSize),
+	readBuffer(new char_type[minReadSize]),
+	readEOF(false),
+	devicePath(devPath){
+		if(fd<0) {
+			perror(devPath.c_str());
+			throw std::runtime_error("Failed to open DOR device "+devPath);
+		}
+		setg(readBuffer,readBuffer,readBuffer);
+	}
 	
 	~DOMIOBuffer(){
 		close(fd);
@@ -98,29 +95,29 @@ public:
 		return(amountWritten);
 	}
 	
-    int underflow(){
-        if(readEOF)
-            return(std::char_traits<char>::eof());
-        if (gptr() < egptr()) // buffer not exhausted
-            return traits_type::to_int_type(*gptr());
-        while(true){
-            waitReady(READ);
-            int result=read(fd,(void*)readBuffer,minReadSize);
-            if(result<0){ //an error occurred
-                if(errno==EAGAIN)
-                    continue;
-                std::cerr << devicePath << ": read gave result " << result;
-                std::cerr << " and error " << errno << std::endl;
-                throw std::runtime_error("Read Error");
-            }
-            else if (result==0)
-                continue;
-            else
-                setg(readBuffer,readBuffer,readBuffer+result); // update the stream buffer
-            break;
-        }
-        return traits_type::to_int_type(*gptr());
-    }
+	traits_type::int_type underflow(){
+		if(readEOF)
+			return(std::char_traits<char>::eof());
+		if (gptr() < egptr()) // buffer not exhausted
+			return(traits_type::to_int_type(*gptr()));
+		while(true){
+			waitReady(READ);
+			int result=read(fd,(void*)readBuffer,minReadSize);
+			if(result<0){ //an error occurred
+				if(errno==EAGAIN)
+					continue;
+				std::cerr << devicePath << ": read gave result " << result;
+				std::cerr << " and error " << errno << std::endl;
+				throw std::runtime_error("Read Error");
+			}
+			else if (result==0)
+				continue;
+			else
+				setg(readBuffer,readBuffer,readBuffer+result); // update the stream buffer
+			break;
+		}
+		return(traits_type::to_int_type(*gptr()));
+	}
 };
 
 #endif //include guard
